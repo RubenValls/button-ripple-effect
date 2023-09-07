46ecfce7997fe7fa4f4309b8cb6410d3f72acca0
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-07 12:14:26 +0200

    ripple effect working correctly, project finished

--- scripts.js
+++ scripts.js
@@ -1,7 +1,21 @@
 const button = document.querySelector('#btn');
 
 button.addEventListener('click', (event) => {
-    
-    console.log(event)
-    console.log(button.getBoundingClientRect())
+    const ripple = document.querySelector('#ripple');
+    ripple.style.height = 1;
+    ripple.style.width = 1;
+    ripple.style.left = event.x - button.getBoundingClientRect().x - 5;
+    ripple.style.top = event.y - button.getBoundingClientRect().y - 7;
+    let expandPixels = 1.75;
+
+    let clickInterval = setInterval(() => {
+        button.disabled = true;
+        ripple.style.transform = `scale(${expandPixels})`
+        expandPixels += 1.75;
+    }, 2);
+    setTimeout(() => { clearInterval(clickInterval); button.disabled = false;}, 1000);
+
+    ripple.style.height = 1;
+    ripple.style.width = 1;
+    button.disabled = false;
 })
